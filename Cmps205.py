4e227b76c07c2546c7118e03aaf1f76d94c8facc
NourKhayata <44363369+NourKhayata@users.noreply.github.com>
2018-11-07 14:46:44 +0200

    Added lines

--- Cmps205.py
+++ Cmps205.py
@@ -4,9 +4,9 @@ database = 'AdventureWorks2016'
 username = 'cmps253'
 password = 'Cmps205!'
 driver= '{SQL Server}'
-
+hello this is code
 cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+password)
-
+file2=open("cmps205.html","w")
 file = open("cmps205.html","w")
 file = open("cmps205.html","w") 
  
@@ -20,13 +20,4 @@ file.write("</head>")
 file.write("<table>")
  
 
-
-cursor = cnxn.cursor()
-cursor.execute("SELECT top 10 * from Person.Person")
-row = cursor.fetchone()
-while row:
-   file.write("<tr><td>" + row[4] + "</td><td>" + row[6] + "</tr>")
-   row = cursor.fetchone()
-file.write("</table>")  
-file.write("</html>") 
-file.close()
+hello word
